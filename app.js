2d3bcf049c2dd2868894b26427598d31c168adae
snt85c <Santi.cacciola@hotmail.it>
2023-01-31 12:54:47 +0000

    add changes for railway

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
-require('dotenv').config()
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
-`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.k6aysbi.mongodb.net/?retryWrites=true&w=majority` ;
+const dev_db_url = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.k6aysbi.mongodb.net/?retryWrites=true&w=majority`;
 
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(mongoDB,{dbName:"minimessageboard"});
+  await mongoose.connect(mongoDB, { dbName: "minimessageboard" });
 }
 
-
-var indexRouter = require('./routes/index');
-var newRouter = require('./routes/new');
+var indexRouter = require("./routes/index");
+var newRouter = require("./routes/new");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/new', newRouter);
+app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
